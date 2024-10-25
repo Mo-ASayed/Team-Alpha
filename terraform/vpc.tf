61296fe11e84241dc94a7fc77e5f3ba5778c8988
Mohammed Sayed <mohammedsayed9607@gmail.com>
2024-10-25 19:42:53 +0100

    updated VPC to include private subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -97,3 +97,56 @@ resource "aws_security_group" "tm_sg" {
     Name = "tm_security_group"
   }
 }
+
+resource "aws_subnet" "tm_private_subnet_1" {
+  vpc_id            = aws_vpc.tm_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "eu-west-2a"
+  tags = {
+    Name = "tm_private_subnet_1"
+  }
+}
+
+resource "aws_subnet" "tm_private_subnet_2" {
+  vpc_id            = aws_vpc.tm_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "eu-west-2b"
+  tags = {
+    Name = "tm_private_subnet_2"
+  }
+}
+
+resource "aws_eip" "tm_nat_eip" {
+}
+
+resource "aws_nat_gateway" "tm_nat_gw" {
+  allocation_id = aws_eip.tm_nat_eip.id
+  subnet_id     = aws_subnet.tm_subnet_1.id
+  tags = {
+    Name = "tm_nat_gateway"
+  }
+}
+
+
+resource "aws_route_table" "tm_private_route_table" {
+  vpc_id = aws_vpc.tm_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.tm_nat_gw.id
+  }
+
+  tags = {
+    Name = "tm_private_route_table"
+  }
+}
+
+resource "aws_route_table_association" "tm_private_rta_1" {
+  subnet_id      = aws_subnet.tm_private_subnet_1.id
+  route_table_id = aws_route_table.tm_private_route_table.id
+}
+
+resource "aws_route_table_association" "tm_private_rta_2" {
+  subnet_id      = aws_subnet.tm_private_subnet_2.id
+  route_table_id = aws_route_table.tm_private_route_table.id
+}
