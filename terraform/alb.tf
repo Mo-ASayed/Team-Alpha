3a9d413487ca64c59b49f10fbc5866ca61ca7168
adnanp97 <adnanp97@outlook.com>
2024-10-25 20:15:45 +0100

    update alb formatting

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,13 +1,9 @@
-# application load balancer
-
-# 1. Define load balancer
-
 resource "aws_lb" "tm_lb" {
   name               = "tm-lb-tf"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.tm_sg.id]
-  subnets            = [faws_subnet.tm_subnet_1.id, aws_subnet.tm_subnet_2.id]
+  subnets            = [aws_subnet.tm_subnet_1.id, aws_subnet.tm_subnet_2.id]
 
   enable_deletion_protection = true
 
@@ -16,22 +12,12 @@ resource "aws_lb" "tm_lb" {
   }
 }
 
-
-# 2. Create Target groups - A target group is required to register your EC2 instances or other 
-# resources that the ALB will forward traffic to.
-
 resource "aws_lb_target_group" "tm_tg" {
   name     = "tm-tg-tf"
-  port     = 80                        
-
+  port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.tm_vpc.id             # The VPC where your ALB and EC2 are running. if name is not main, add name
-
+  vpc_id   = aws_vpc.tm_vpc.id
 }
-
-
-# 3. Set listeners - A listener defines how the ALB forwards traffic (e.g., HTTP/HTTPS). 
-# You'll need at least one listener, usually for HTTP (port 80) or HTTPS (port 443)
 
 resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.tm_lb.arn
@@ -40,27 +26,24 @@ resource "aws_lb_listener" "http_listener" {
 
   default_action {
     type = "redirect"
-    
+
     redirect {
-      protocol = "HTTPS"
-      port     = "443"
+      protocol    = "HTTPS"
+      port        = "443"
       status_code = "HTTP_301"
     }
   }
 }
 
-# 4. HTTPs listeners - 
-
-
 resource "aws_lb_listener" "https_listener" {
   load_balancer_arn = aws_lb.tm_lb.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"  # AWS recommended SSL policy
-  certificate_arn   = aws_acm_certificate.my_cert.arn  # ARN of your SSL certificate
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.my_cert.arn
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.tm_tg.arn  # Forward traffic to the target group
+    target_group_arn = aws_lb_target_group.tm_tg.arn
   }
 }
